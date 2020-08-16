ded8d91adbad17357ab61969086bdaf678f73287
Spencer Small <641091+ssmall@users.noreply.github.com>
2020-08-16 06:56:44 +0000

    Got Zigbee and Z-Wave ports backwards :(

--- start.sh
+++ start.sh
@@ -13,7 +13,7 @@ check_variable () {
 check_variable SOCAT_ZIGBEE_ADDRESS ${SOCAT_ZIGBEE_ADDRESS}
 check_variable SOCAT_ZWAVE_ADDRESS ${SOCAT_ZWAVE_ADDRESS}
 
-socat -d -d -d -lf /var/log/ttyUSB0.log pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:${SOCAT_ZIGBEE_ADDRESS} &
-socat -d -d -d -lf /var/log/ttyUSB1.log pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:${SOCAT_ZWAVE_ADDRESS} &
+socat -d -d -d -lf /var/log/ttyUSB0.log pty,link=/dev/ttyUSB0,raw,user=0,group=0,mode=777 tcp:${SOCAT_ZWAVE_ADDRESS} &
+socat -d -d -d -lf /var/log/ttyUSB1.log pty,link=/dev/ttyUSB1,raw,user=0,group=0,mode=777 tcp:${SOCAT_ZIGBEE_ADDRESS} &
 
 python -m homeassistant ${PROGRAM_ARGS}
